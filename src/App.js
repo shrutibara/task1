7f43df1767d065b39bab21400726ae3109e9946a
shruti0802 <137978020+shrutibara@users.noreply.github.com>
2023-11-04 00:50:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; // Import React
+import './App.css';
+import Translate from './components/Translate';
+
+function App() {
+  return (
+    <>
+    <h1 className='h2'>Text Translater</h1>
+      <Translate />
+    </>
+  );
+}
+
+export default App;
